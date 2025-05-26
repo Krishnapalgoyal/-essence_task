63c2ae6dc1ec64371ff0a8c362061f4343e9d43f
krishnapal Goyal <krishnapal1224@gmail.com>
2025-05-26 17:11:17 +0530

    add cancancan for authrization

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { admin: 0, service_provider: 1, customer: 2 }
 end
